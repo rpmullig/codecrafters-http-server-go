e7de396cde9f2f89678d867c85054cfa0d98f82b
rpmullig <robertpmulligan@gmail.com>
2023-11-14 00:38:27 -0500

    version issue

--- app/server.go
+++ app/server.go
@@ -44,7 +44,8 @@ func main() {
 	if path == "/" {
 		conn.Write([]byte(HTTP_200_OK + CRLF + CRLF))
 	} else if strings.HasPrefix(path, ECHO_PREFIX) {
-		response_body, _ := strings.CutPrefix(path, ECHO_PREFIX)
+		// response_body, _ := strings.CutPrefix(path, ECHO_PREFIX) available in version 1.20+
+		var response_body string = path[len(ECHO_PREFIX):]
 		conn.Write([]byte(HTTP_200_OK + CRLF + CONTENT_TYPE_TEXT + CRLF + "Content-Length: " + len(response_body) + CRLF + CRLF + response_body))
 	} else {
 		conn.Write([]byte(HTTP_404_NOT_FOUND + CRLF + CRLF))
