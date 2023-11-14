db2db1be11bedb794e254623c1c5aa873985513b
rpmullig <robertpmulligan@gmail.com>
2023-11-13 23:45:47 -0500

    rewritting it to see if I can fix

--- app/server.go
+++ app/server.go
@@ -12,19 +12,18 @@ func main() {
 
 	fmt.Println("Logs from your program will appear here!")
 
-	 l, err := net.Listen("tcp", "0.0.0.0:4221")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 4221")
-	 	os.Exit(1)
-	 }
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
 
-	 conn, err := l.Accept()
-	 if err != nil {
+	conn, err := l.Accept()
+	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
-	 }
+	}
+	defer conn.Close()
 
-	
 	conn.Write([]byte(HTTP_200_OK + CRLF + CRLF))
-	conn.Close()
 }
