e4049241b42018bf3349c1898b516b064573ad9c
rpmullig <robertpmulligan@gmail.com>
2023-11-13 23:31:51 -0500

    Cheated, but I am not as strong in Go.

--- app/server.go
+++ app/server.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	const string CRLF = "\r\n"
+	const string HTTP_200_OK = "HTTP/1.1 200 OK"
+
 	fmt.Println("Logs from your program will appear here!")
 
 	 l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -14,10 +17,13 @@ func main() {
 	 	fmt.Println("Failed to bind to port 4221")
 	 	os.Exit(1)
 	 }
-	
-	 _, err = l.Accept()
+
+	_, err = l.Accept()
 	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	 }
-}
+
+	defer conn.Close()
+	conn.Write([]byte(HTTP_200_OK + CRLF + CRLF))
+ }
